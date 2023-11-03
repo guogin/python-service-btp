a5328f212f54b198e09ef23ea210561843a327c7
Guo, Yong Kang <yong.kang.guo@sap.com>
2023-11-03 21:16:26 +0800

    Python Service @BTP

---
+++ server.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+port = int(os.environ.get('PORT', 3000))
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)
